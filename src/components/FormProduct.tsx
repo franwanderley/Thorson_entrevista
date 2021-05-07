fe41d7e7307d15584ce240db810bcffcdb8e255c
franwanderley <franwanderley@outlook.com>
2021-05-07 17:14:09 -0300

    ultilizando React-Hok-Form

--- src/components/FormProduct.tsx
+++ src/components/FormProduct.tsx
@@ -1,9 +1,11 @@
 import { 
    Button, InputLabel, MenuItem, Select, TextField 
 } from '@material-ui/core';
-import React, { FormEvent, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { FaSave } from 'react-icons/fa';
 import swal from 'sweetalert';
+import {Controller, useForm} from 'react-hook-form';
+
 import { categorias } from '../libs/categoria';
 import { FormMain } from '../styles/Style';
 import { Product, ProductContext } from './../context/ProductContext';
@@ -16,19 +18,35 @@ interface FormProductProps{
 
 export function FormProduct({product, isEditable, setIsEditable}: FormProductProps){
    const {onEdit, onSave} = useContext(ProductContext);
-   const [sku, setSku] = useState<number>();
-   const [nome, setNome] = useState<string>();
-   const [preco, setPreco] = useState<string>();
-   const [categoria, setCategoria] = useState<string>();
+   const { handleSubmit, control, setValue,reset } = useForm<Product>();
 
-   function handleSubmit(form : FormEvent){
-      form.preventDefault();
-      const newProduct = { 
-         sku: sku || 0,
-         nome: nome || '',
-         preco: preco || '', 
-         categoria: categoria || '' 
-      };
+   async function onSubmit(newProduct : Product){
+      //Validação
+      const valor = Number(newProduct.preco);
+      if(valor <= 0){
+         await swal({
+            title: 'Não foi possivel salvar o produto!', 
+            text: 'Preços iguais ou menor do que 0 não são permitidos', 
+            icon: 'warning'
+         });
+         return ;
+      }
+      if(newProduct.sku <= 0){
+         await swal({
+            title: 'Não foi possivel salvar o produto!', 
+            text: 'Codigo SKU iguais ou menor do que 0 não são permitidos', 
+            icon: 'warning'
+         });
+         return ;
+      }
+      if(newProduct.sku.toString().includes('.') || newProduct.sku.toString().includes(',')){
+         await swal({
+            title: 'Não foi possivel salvar o produto!', 
+            text: 'Pontos e Virgulas não são permitidos no codigo SKU', 
+            icon: 'warning'
+         });
+         return ;
+      } 
 
       if(isEditable){
          onEdit(newProduct);
@@ -40,69 +58,92 @@ export function FormProduct({product, isEditable, setIsEditable}: FormProductPro
          }catch(error){
             swal({
                title: 'Não foi possivel salvar o produto',
-               text: `Codigo SKU ${sku} já existe`,
+               text: `Codigo SKU ${newProduct.sku} já existe`,
                icon: 'warning',
             });
          }
-      }
+      } 
       //Limpar os campos
-      setSku(0);
-      setNome('');
-      setPreco('');
-      setCategoria('');
+      reset();
   }
 
   useEffect(() => {
      if(isEditable){
-        setSku(product?.sku);
-        setNome(product?.nome);
-        setPreco(product?.preco);
-        setCategoria(product?.categoria);
+        console.log(product);
+        product?.sku && setValue('sku', product?.sku);
+        product?.nome && setValue('nome', product?.nome);
+        product?.preco && setValue('preco', product?.preco);
+        product?.categoria && setValue('categoria', product?.categoria);
      }
-  }, [product,isEditable]);
+  }, [product, isEditable, setValue]);
 
    return (
-      <FormMain onSubmit={handleSubmit} >
-          <TextField 
-            style={{marginBottom: 10, width: '20%'}}
-            id="standard-basic"
-            value={sku}
-            type="number"
-            label="codigo do SKU"
-            onChange={e => setSku(Number(e.target.value))} 
-            required
-          />
-          <TextField
-            style={{marginBottom: 10, width: '20%'}} 
-            id="standard-basic"
-            value={nome} 
-            type="text" 
-            label="Nome do Produto"
-            onChange={e => setNome(e.target.value)} 
-            required
-          />
-          <TextField
-            style={{marginBottom: 10, width: '20%'}} 
-            id="standard-basic" 
-            value={preco}
-            type="text" 
-            label="Preço"
-            onChange={e => setPreco(e.target.value)}
-            required
-          />
+      <FormMain onSubmit={handleSubmit(onSubmit)} >
+         <Controller
+            name="sku"
+            control={control}
+            rules={{ required: true }}
+            defaultValue=""
+            render={({ field }) => (
+            <TextField 
+               style={{marginBottom: 10, width: '20%'}}
+               id="standard-basic" 
+               type="text"
+               label="Codigo SKU"
+               {...field}
+               disabled={isEditable}
+            />)}
+         />
+         <Controller
+            name="nome"
+            control={control}
+            defaultValue=""
+            rules={{ required: true, minLength: 5 }}
+            render={({ field }) => (
+            <TextField
+               style={{marginBottom: 10, width: '20%'}} 
+               id="standard-basic"
+               type="text" 
+               label="Nome do Produto"
+               required
+               {...field}
+             />)}
+         />
+         <Controller
+            name="preco"
+            control={control}
+            defaultValue=""
+            rules={{required: true }}
+            render={({ field }) => (
+               <TextField
+               style={{marginBottom: 10, width: '20%'}} 
+               id="standard-basic"
+               type="number" 
+               label="Preço"
+               
+               required
+               {...field}
+             />)}
+         />
         <InputLabel id="demo-simple-select-label" style={{marginBottom: 10, width: '20%'}}>Escolha uma Categoria</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={categoria}
-          style={{marginBottom: 15, width: '20%'}}
-          onChange={e => setCategoria(e.target.value as string)}
-          required
-        >
-          {categorias.map(c => (
+        <Controller
+            name="categoria"
+            control={control}
+            defaultValue=""
+            rules={{ required: true }}
+            render={({ field }) => (
+            <Select
+            style={{marginBottom: 15, width: '20%'}}
+               labelId="demo-simple-select-label"
+               id="demo-simple-select"
+               required
+               {...field}
+            >  
+            {categorias.map(c => (
               <MenuItem key={c} value={c}>{c}</MenuItem>
-          ))}
-        </Select>
+            ))}
+        </Select>)}
+         />
         <Button variant="contained" type="submit" color="primary">
           <FaSave style={{marginRight: 5}}/> Salvar
         </Button>
