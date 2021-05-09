5ed9ed20d09952beb5458af315a2c81d537c2784
franwanderley <franwanderley@outlook.com>
2021-05-08 22:45:46 -0300

    corrigindo erros do servidor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-   baseURL: 'http://localhost:3333',
-   //https://my-json-server.typicode.com/franwanderley/Thorson_entrevista
+   baseURL: 'https://my-json-server.typicode.com/franwanderley/Thorson_entrevista',
 })
